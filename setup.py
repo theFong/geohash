e0732a6a17c11ae65ee2c611478247faf60e6566
Alec Fong <alecsanf@usc.edu>
2019-11-14 10:38:51 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ License along with Geohash.  If not, see
 from setuptools import setup, find_packages
 setup(
     name = "Geohash",
-    version = "1.0",
+    version = "0.1.0",
     packages = find_packages(),
 
     # Project uses reStructuredText, so ensure that the docutils get
